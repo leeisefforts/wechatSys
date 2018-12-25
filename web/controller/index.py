22c730b1ba998fe38420fa00f884a160b1c98fa8
Bryant Lee <leeisefforts@163.com>
2018-12-25 14:56:06 +0800

    'add'

--- web/controller/index.py
+++ web/controller/index.py
@@ -19,7 +19,7 @@ def callback():
     state = req['state'] if 'state' in req else ''
     config_mina = app.config['MINA_APP']
     url = 'https://dopen.weimob.com/fuwu/b/oauth2/token?code={0}&grant_type=authorization_code&client_id={1}&client_secret={2}&redirect_uri={3}'.format(
-        code, config_mina['appid'], config_mina['appkey'], 'http://cweiglobal.com/login')
+        code, config_mina['appid'], config_mina['appkey'], 'http://47.104.176.254/api/callback')
     r = requests.post(url=url)
     data = json.loads(r.text)
     return jsonify(data)
