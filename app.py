906672b1e002dfd5b40e56c0ce6086a825cdff8c
Bryant Lee <leeisefforts@163.com>
2018-12-29 14:21:09 +0800

    'add'

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from application import app, manager
 from flask_script import Server
-from flask_cors import CORS
 import www
-CORS(app, resources=r'/*')
+
 # web server
 manager.add_command("runserver",
                     Server(host='127.0.0.1', port=app.config['SERVER_PORT'], use_debugger=True, use_reloader=True))
